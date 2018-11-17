10d4c4f514f4ed57e461fef8899adcdb62b65574
Sam Otto <moffotto@Sams-MacBook-Pro.local>
2018-11-17 14:50:30 -0800

    Updates

---
+++ precache-manifest.536c94933f6518da242424f898e47e40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af261cb6ba5f7ee021694d2ee521bcee",
+    "url": "/ps7-moffotto/static/media/cards.af261cb6.csv"
+  },
+  {
+    "revision": "c8188755bc216bf3dc9d",
+    "url": "/ps7-moffotto/static/js/runtime~main.c8188755.js"
+  },
+  {
+    "revision": "f237cace6b6720326ba3",
+    "url": "/ps7-moffotto/static/js/main.f237cace.chunk.js"
+  },
+  {
+    "revision": "194d62dd1b203498d320",
+    "url": "/ps7-moffotto/static/js/1.194d62dd.chunk.js"
+  },
+  {
+    "revision": "f237cace6b6720326ba3",
+    "url": "/ps7-moffotto/static/css/main.1aab5d75.chunk.css"
+  },
+  {
+    "revision": "e3feda8aa5a9a0dda8b908e52d9b7f03",
+    "url": "/ps7-moffotto/index.html"
+  }
+];
